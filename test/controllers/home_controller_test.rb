b011c5190234c3d6768031bee992ca93586702c6
Jens Schmidt <jens.schmidt@actinoide.de>
2026-05-25 23:54:10 +0200

    Show signed-in home state

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,5 +6,17 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select "h1", I18n.t("home.index.title")
+    assert_select "a[href=?]", new_session_path, text: I18n.t("home.index.sign_in")
+  end
+
+  test "shows signed-in state after authentication" do
+    user = users(:one)
+    sign_in_as(user)
+
+    get root_path
+
+    assert_response :success
+    assert_select "p", text: I18n.t("home.index.signed_in_as", email: user.email_address)
+    assert_select "a[href=?]", new_session_path, count: 0
   end
 end
