2190d29f6696b40ac7df6d013d74238b55437908
Jens Schmidt <jens.schmidt@actinoide.de>
2026-05-27 23:44:14 +0200

    Align mobile menu with log action

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -22,7 +22,10 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "html.theme-dark"
     assert_select "[data-testid=app-navigation]"
-    assert_select "[data-testid=app-mobile-menu]"
+    assert_select "[data-testid=app-mobile-actions].md\\:hidden" do
+      assert_select "a[data-testid=app-nav-log][href=?]", new_brew_path
+      assert_select "[data-testid=app-mobile-menu]"
+    end
     assert_select "[data-testid=app-desktop-navigation]"
     assert_select "[data-testid=app-mobile-navigation]", count: 0
     assert_select "[data-testid=app-nav-more]", count: 0
